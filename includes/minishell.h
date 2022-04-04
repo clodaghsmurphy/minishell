3ce8e55100735c00620dbb1ba0ece2bacc8a0515
Clodagh MURPHY <clmurphy@e2r3p1.clusters.42paris.fr>
2022-04-04 12:11:30 +0200

    added inlcudes + header file and created main file in src folder

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clmurphy <clmurphy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 12:06:00 by clmurphy          #+#    #+#             */
+/*   Updated: 2022/04/04 12:10:53 by clmurphy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+#endif
