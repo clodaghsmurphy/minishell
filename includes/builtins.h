dd137c125435ea70928191958c3b5fb9bef22881
Clodagh MURPHY <clmurphy@e2r11p21.clusters.42paris.fr>
2022-04-13 15:18:17 +0200

    moved all functions to minishell.h and cleaned some exec functions to compile

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,11 +6,7 @@
 /*   By: clmurphy <clmurphy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 16:15:52 by amontant          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/04/13 15:04:48 by clmurphy         ###   ########.fr       */
-=======
-/*   Updated: 2022/04/13 14:56:42 by amontant         ###   ########.fr       */
->>>>>>> 9f7997886c58acfb8134be2c62903f7c9511f487
+/*   Updated: 2022/04/13 15:16:51 by clmurphy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +14,4 @@
 # define BUILTINS_H
 # include "minishell.h"
 
-<<<<<<< HEAD
-=======
-void	del_one(t_env **lst, t_env *to_del);
-char	*get_value(char *line);
-char	*get_name(char *line);
-t_env	*env_new(char *name, char *value);
-void	env_add_back(t_env **alst, t_env *new_elem);
-void	env_free(t_env *env);
-void	export_variable(t_env **env, char *new_v);
-void	ft_export(t_env **env, char **params);
-int		check_valid_variable(char *variable);
-void	print_env(t_env *env);
-void	del_one(t_env **lst, t_env *to_del);
-void	check_rm_double(t_env **env);
-void	ft_unset(t_env	**env, char **params);
-void	unset_variable(t_env **env, char *variable_name);
-void	cd(char *path);
-void	pwd(void);
-int		is_valid_opt(char *arg);
-int		get_opt(char **args);
-
->>>>>>> 9f7997886c58acfb8134be2c62903f7c9511f487
 #endif
