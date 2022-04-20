94182c972c05b82e1076732902b1d4c00db3a54a
shiloub <achillemo@hotmail.fr>
2022-04-20 16:47:31 +0200

    message

--- exec/exec.c
+++ exec/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amontant <amontant@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shiloub <shiloub@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 14:02:54 by amontant          #+#    #+#             */
-/*   Updated: 2022/04/19 21:34:48 by amontant         ###   ########.fr       */
+/*   Updated: 2022/04/20 16:44:45 by shiloub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,7 @@ void	ft_exe(t_mshell *mini)
 		exe_builtins(mini->command->value, &mini->env);
 	else
 	{
-		//pid = fork();
-		//if (pid == 0)
-		//{
-			exec_cmd(mini->env, mini->command, mini->command);
-		//}
-		//waitpid(pid, 0, 0);
+		exec_cmd(mini->env, mini->command, mini->command);
 	}
 }
 
@@ -75,26 +70,24 @@ void	exec_cmd(t_env *env, t_command *command, t_command *current)
 {
 	char	*path;
 	int		pid;
-	int		tab_pid[100];
-
-	int i = 0;
-	while (i < 100)
-		tab_pid[i++] = 0;
-	i = 0;
+	int		*pipe_fd;
+	int		i = 0;
 	
+	pipe_fd = malloc(sizeof(int) * cmd_list_size(command) * 2);
+	while (i < (cmd_list_size(command) - 1))
+	{
+		pipe(pipe_fd + i * 2);
+		i ++;
+	}
+	pipe_fd[i * 2] = 0;
+	i = 0;
+	wait(0);
 	while (current)
 	{
-		if (current->next)
-		{
-			if (pipe(current->next->pipe_fd) == -1)
-			exit(0);
-		}
 		pid = fork();
-		if (pid != 0)
-			tab_pid[i++] = pid;
 		if (pid == 0)
 		{
-			ft_dup(command, current);
+			ft_dup(command, current, pipe_fd, i);
 			if (is_builtins(current->value))
 			{
 				exe_builtins(current->value, &env);
@@ -104,36 +97,31 @@ void	exec_cmd(t_env *env, t_command *command, t_command *current)
 			execve(path, current->value, env_to_tab(env));
 			exit(0);
 		}
+		i += 2;
 		current = current->next;
 	}
 	i = 0;
-	while (tab_pid[i] != 0)
-	{
-		printf("j'attend pid %d et je suis pid %d\n", tab_pid[i], pid);		
-		waitpid(tab_pid[i++], 0, 0);
-		printf("c bon !\n");
-	}
+	while (pipe_fd[i])
+		close(pipe_fd[i++]);
+	while (waitpid(-1, 0, 0) != -1)
+		i++;
 }
 
-void	ft_dup(t_command *command, t_command *current)
+void	ft_dup(t_command *command, t_command *current, int *pipe_fd, int i)
 {
+	int	j;
+	j = 0;
 	if (cmd_lst_pos(command, current) == 1 && cmd_list_size(command) > 1)
-	{
-		close(current->next->pipe_fd[0]);
-		dup2(current->next->pipe_fd[1], 1);
-	}
+		dup2(pipe_fd[1], 1);
 	else if (cmd_lst_pos(command, current) == cmd_list_size(command))
-	{
-		close(current->pipe_fd[1]);
-	 	dup2(current->pipe_fd[0], 0);
-	}
+	  	dup2(pipe_fd[i - 2], 0);
 	else
 	{
-		close(current->next->pipe_fd[0]);
-		close(current->pipe_fd[1]);
-		dup2(current->pipe_fd[0], 0);
-		dup2(current->next->pipe_fd[1], 1);
+	 	dup2(pipe_fd[i - 2], 0);
+	 	dup2(pipe_fd[i + 1], 1);
 	}
+	while (pipe_fd[j])
+		close(pipe_fd[j++]);
 }
 char	**env_to_tab(t_env *env)
 {
